304f9e3e12d7895821862000e0b1244b3667aa81
ivankras <ivankras@hotmail.com>
2021-11-10 17:17:03 -0600

    Airline & Rating

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,114 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import Header from "./Header";
+import ReviewForm from "./ReviewForm";
+import Review from "./Review";
 
-const Airline = () => {
-    return <div>This is the Airlines#show</div>
+const Wrapper = styled.div`
+    margin-left:auto;
+    margin-right:auto;
+    display:grid;
+    grid-template-columns: repeat(2, 1fr);
+`
+
+const Column = styled.div`
+    background: #fff;
+    height: 100vh;
+    overflow: scroll;
+
+    &:last-child {
+        background: #000;
+    }
+`
+
+const Main = styled.div`
+    padding-left: 50px;
+`
+
+const Airline = (props) => {
+    const [airline, setAirline] = useState({});
+    const [review, setReview] = useState({});
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        const slug = props.match.params.slug;
+        const url = `/api/v1/airlines/${slug}`;
+
+        axios.get(url)
+            .then(resp => {
+                setAirline(resp.data);
+                setLoaded(true);
+            })
+            .catch(resp => console.log(resp))
+    }, []);
+
+    const handleChange = e => {
+        e.preventDefault();
+
+        setReview(Object.assign({}, review, {[e.target.name]: e.target.value}));
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const csrfToken = document.querySelector('[name=csrf-token]').textContent;
+        axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
+
+        const airline_id = airline.data.id;
+        axios.post('/api/v1/reviews', {review, airline_id})
+            .then(r => {
+                const included = [...airline.included, resp.data.data];
+                setAirline({...airline, included});
+                setReview({title: '', description: '', score: 0});
+            })
+            .catch(r => {});
+    };
+
+    const setRating = (score, e) => {
+        e.preventDefault();
+        setReview({...review, score});
+    };
+
+    let reviews;
+    if (loaded && airline.included) {
+        reviews = airline.included.map((item, index) => {
+            return (
+                <Review
+                    key={index}
+                    attributes={item.attributes}
+                />
+            );
+        });
+    }
+
+    return (
+        <Wrapper>
+            {
+                loaded &&
+                <Fragment>
+                    <Column>
+                        <Main>
+                            <Header
+                                attributes={airline.data.attributes}
+                                reviews={airline.included}
+                            />
+                        <div className="reviews">{reviews}</div>
+                    </Main>
+                    </Column>
+                    <Column>
+                        <ReviewForm
+                            handleChange={handleChange} 
+                            handleSubmit={handleSubmit}
+                            setRating={setRating}
+                            attributes={airline.data.attributes}
+                            review={review} 
+                        />
+                    </Column>
+                </Fragment>
+            }
+        </Wrapper>
+    )
 };
 
 export default Airline;
